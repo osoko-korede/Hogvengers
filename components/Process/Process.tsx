d76ee067b707cf086d5b8372e2315218df647dd2
Korede <osokokoredepeter@gmail.com>
2025-03-28 23:27:26 +0100

    updated how to buy

--- components/Process/Process.tsx
+++ components/Process/Process.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Process = () => {
   return (
-    <div className='h-screen bg-[#EC1F6A]'>How to</div>
+    <div className='h-screen bg-[#EC1F6A]'>How to Buy</div>
   )
 }
 
